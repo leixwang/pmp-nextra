d1f74e598f5a14f89d16ace9494f88c787087093
Richard <richard@newborntown.com>
2023-01-29 23:15:41 +0800

    Update head.

--- theme.config.jsx
+++ theme.config.jsx
@@ -7,6 +7,13 @@ export default {
     prev: true,
     next: true
   },
+	head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next docs builder" />
+      <meta name="og:title" content="Nextra: the next docs builder" />
+    </>
+  ),
   footer: {
     text: <span>
       MIT 2018 - {new Date().getFullYear()} © <a href="https://leix.wang" target="_blank">Leix.Wang</a>.
